47f951d51272a7594f540af54c0b04fd5090f6f2
ieremius <hibariyashiki@gmail.com>
2026-08-10 13:29:39 +0900

    Add UniversitiesController, views, helper, seeds, and controller tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+University.create!([
+  { name: "早稲田大学",   short_name: "早大", slug: "waseda" },
+  { name: "慶應義塾大学", short_name: "慶大", slug: "keio" },
+  { name: "明治大学",     short_name: "明大", slug: "meiji" },
+  { name: "法政大学",     short_name: "法大", slug: "hosei" },
+  { name: "東京大学",     short_name: "東大", slug: "tokyo" },
+  { name: "立教大学",     short_name: "立大", slug: "rikkyo" }
+])
