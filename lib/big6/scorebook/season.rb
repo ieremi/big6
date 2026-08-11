a105299a82773326ad48ef6e73e42aee980580ed
ieremius <hibariyashiki@gmail.com>
2026-08-11 03:17:40 +0000

    import

--- lib/big6/scorebook/season.rb
+++ lib/big6/scorebook/season.rb
@@ -4,12 +4,16 @@ require "json"
 
 url = URI("https://big6scorebook.jp/api/game/search")
 
-url.query = URI.encode_www_form(
-  league: "リーグ戦",
-  season: "1997秋"
-)
+(1925..2026).each do |year|
+  [ "春", "秋" ].each do |term|
+    season = year.to_s + term
+    url.query = URI.encode_www_form(
+      league: "リーグ戦",
+      season: season
+    )
+    response = Net::HTTP.get_response(url)
 
-response = Net::HTTP.get_response(url)
-
-puts response.body
-data = JSON.parse(response.body)
+    data = JSON.parse(response.body)
+    p data.dig("data", 0, "leagueSeason")
+  end
+end
