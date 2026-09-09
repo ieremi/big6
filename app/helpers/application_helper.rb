0c297be1185a451003a2151450f6b83fec372f10
ieremius <hibariyashiki@gmail.com>
2026-09-09 07:43:19 +0000

    games view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def score_span(team0_score, team1_score)
+    tag.span class: "score" do
+      tag.span(team0_score, class: "score-num score-num-left") +
+        tag.span("-", class: "score-sep") +
+        tag.span(team1_score, class: "score-num score-num-right")
+    end
+  end
 end
